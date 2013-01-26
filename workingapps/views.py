7b54733df9c50a3a5290252ecdf5957b38799b96
Victor Cabral <vccabral@gmail.com>
2013-01-26 13:03:06 -0500

    stubbed out command

--- workingapps/views.py
+++ workingapps/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from workingapps.models import Project
 
 def index(request):
+    projects = Project.objects.filter(admin=request.user)
     return render_to_response('projects.html', locals(), context_instance=RequestContext(request))
